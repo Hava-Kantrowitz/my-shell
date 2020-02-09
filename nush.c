f3b42361549882a7011eea5d451b4495be9cebaa
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2020-02-09 16:29:07 -0500

    Add files via upload

--- nush.c
+++ nush.c
@@ -9,54 +9,99 @@
 #include "list.h"
 #include "tokens.h"
 
+int
+printToFile(char* args[]) {
+		int new_pid;
+		int j = 0; 
+		char* commandArgs[256];
+		while (*args[j] != '>') {
+			commandArgs[j] = args[j];
+			j++;
+		}
+
+		commandArgs[j] = NULL;
+
+		if ((new_pid = fork())) {
+			int status;
+			waitpid(new_pid, &status, 0);
+		}
+
+		else {
+			char* file = args[j+2]; 
+			int fd = open(file, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+			close(1);
+			dup(fd);
+			close(fd); 
+			execvp(commandArgs[0], commandArgs); 
+		}
+	return 0; 
+
+}
+
+int
+getFromFile(char* args[]) {
+	return 0;
+}
+
+int
+pipeInput(char* args[]) {
+	return 0;
+}
+
 void
 execute(char* cmd)
 {
     int cpid;
 
     if ((cpid = fork())) {
-        // parent process
-        //printf("Parent pid: %d\n", getpid());
-        //printf("Parent knows child pid: %d\n", cpid);
-
-        // Child may still be running until we wait.
 
         int status;
         waitpid(cpid, &status, 0);
 
-        //printf("== executed program complete ==\n");
-
-        //printf("child returned with wait code %d\n", status);
         if (WIFEXITED(status)) {
             //printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
         }
     }
     else {
-        // child process
-        //printf("Child pid: %d\n", getpid());
-        //printf("Child knows parent pid: %d\n", getppid());
-
-        /*for (int ii = 0; ii < strlen(cmd); ++ii) {
-            if (cmd[ii] == ' ') {
-                cmd[ii] = 0;
-                break;
-            }
-        }*/
 
 	//tokenize
 	
 	list* tokens = tokenize(cmd);
 	char* args[256]; 
 	int i = 0;
-	char* progName;
+	int hasSemi = 0; 
+	int hasRedirOut = 0;
 	for (; tokens; tokens = tokens->tail) {
-		{
 		args[i] = tokens->head;
-		i++;
+		if (*args[i] == ';') {
+			hasSemi = 1;
 		}
+		if (*args[i] == '>') {
+			hasRedirOut = 1;
+		}
+		i++;
 	}
 
 	args[i] = NULL;
+	if (hasSemi) {
+		//handle the semicolon
+	}
+
+	if (hasRedirOut) {
+		printToFile(args); 
+	}
+
+	if (hasRedirIn) {
+		//getFromFile(args);
+	}
+
+	if (hasPipe) {
+		pipeInput(args);
+	}
+
+	if (hasBackground) {
+		//background(args);
+	}
 
 
 	//process
@@ -66,9 +111,10 @@ execute(char* cmd)
 
         //printf("== executed program's output: ==\n");
 
+	else {
 	execvp(args[0], args);
+	}
 
-        printf("Can't get here, exec only returns on error.");
     }
 }
 
